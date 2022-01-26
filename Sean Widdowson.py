bc71623838c2ebffe06d00ff29461dba91d06cf2
SeanTheShe3p <98074937+SeanTheShe3p@users.noreply.github.com>
2022-01-26 00:32:20 -0600

    Add files via upload

---
+++ Sean Widdowson.py
@@ -0,0 +1,68 @@
+# 2.3
+print("Sean Widdowson")
+print("Mobile Design and Development")
+
+# 2.3
+print('The dog says "woof!"')
+
+# 2.4
+Age = 26
+print(Age)
+print("age ", Age)
+Age = 42
+print("age ", Age)
+
+# 2.6
+name = input("What is your name? ")
+print("Hello, ", name)
+
+# 2.6-2.7
+age = int(input("How old are you?"))
+print("This year you are ", age)
+age = age + 1
+str(age)
+print("Next year you will be ", age)
+
+# 2.7
+answer = 7 / 2
+print(answer)
+answer = 7 // 2
+remainder = 7 % 2
+print(answer)
+print(answer, "R", remainder)
+
+# 2.7
+print("3 + 3 = ", str(3 + 3))  # problem 0
+print("3 / 3 = ", str(3 / 3))  # problem 1
+#  print(3 // 3)
+#  divide = 3 // 3
+#  print(divide)
+#  print(2.5 / 6)
+print("2.5 / 2.5 = ", str(2.5 / 2.5))  # problem 2
+print("2.8 / 2 = ", str(2.8 / 2))  # problem 3
+#  print(2.8 / 2)
+
+# 2.8
+print("one", end=" ")
+print("two", end=" ")
+print("three")
+
+# 2.8
+print("sunday\tmonday\ttuesday\twednesday\tthursday\tfriday\tsaturday")
+
+# 2.8
+# name = str(input("What is your name?"))
+# hello = "hello"
+# greeting = hello + name
+# print(str(greeting))
+name = input("what is your name?")
+print("Hello " + name)
+# print(str("hello " + name))
+
+# 2.8
+number = 6548974897.5687979797
+print(format(number, '30,.2f'))
+
+# 2.8
+percentage = 7654
+print(format(percentage, '.2%'))
