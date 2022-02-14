334169260460c9ade83d686346050a09ad3578fe
SeanTheShe3p <98074937+SeanTheShe3p@users.noreply.github.com>
2022-02-14 16:19:00 -0600

    Add files via upload

---
+++ 99 bottles.py
@@ -0,0 +1,11 @@
+max_beer = int(input("number of beers"))
+for beer in range(max_beer, 1, -1):
+    print(str(beer), " bottles of beer on the wall,")
+    print(str(beer), " bottles of beer!")
+    print("take one down pass it around,")
+    print(str(beer - 1), " bottles of beer on the wall")
+for beer in [1]:
+    print(str(beer), " bottle of beer on the wall,")
+    print(str(beer), " bottle of beer!")
+    print("take one down pass it around,")
+    print("no more bottles of beer on the wall")
