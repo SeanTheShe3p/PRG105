6e2330f23489a4c324aca62881c2560e2d18bb6d
SeanTheShe3p <98074937+SeanTheShe3p@users.noreply.github.com>
2022-02-21 19:04:28 -0600

    Add files via upload

---
+++ menu.py
@@ -0,0 +1,39 @@
+def main():
+    print('hello, welcome to reasons to be vegan.')
+    print('please select a meat source to find out more about it')
+    print('A: chicken\nB: beef\nC: pork\nD: fish\nE: people')
+    selection = input('what would you like to learn not to eat?')
+    if selection == 'a' or selection == 'A':
+        chicken()
+    if selection == 'b' or selection == 'B':
+        beef()
+    if selection == 'c' or selection == 'C':
+        pork()
+    if selection == 'd' or selection == 'D':
+        fish()
+    if selection == 'e' or selection == 'E':
+        people()
+    print('\n*note: your diet is your opinion, this is meant to be humorous')
+
+
+def chicken():
+    print('this is not a food because you dont like chicken nuggets')
+
+
+def beef():
+    print('this is not a food because hamburgers are nasty')
+
+
+def pork():
+    print('this is not a food because bacon is mainly fat')
+
+
+def fish():
+    print('this is not a food because you cant swim')
+
+
+def people():
+    print('this is not a food because that''s cannibalism')
+
+
+main()
