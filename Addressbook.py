3d6c273011b964ebe07e01db2854e3514469f5eb
SeanTheShe3p <98074937+SeanTheShe3p@users.noreply.github.com>
2022-02-28 22:42:45 -0600

    Add files via upload

---
+++ Addressbook.py
@@ -0,0 +1,12 @@
+def main():
+    number = int(input('How many people are you adding?'))
+    address = open('addresses.txt', 'w')
+    for person in range(1, number + 1):
+        name = input('what is the name of person ')
+        phone_num = input('what is the phone number of person')
+        email_add = input('what is their email address?')
+        address.write(name + ' :: ' + email_add + ' :: ' + phone_num + '\n')
+    address.close()
+
+
+main()
